ba9ed4df9a415932e14a78fd329c94aec57c26bc
Elle Yoko Suzuki <elle.suzuki@gmail.com>
2013-02-17 13:06:12 -0800

    set default attributes

--- lib/about-suzuki/suzuki.rb
+++ lib/about-suzuki/suzuki.rb
@@ -1,9 +1,9 @@
 class Suzuki
 	attr_reader :name, :fave_color, :fave_food
 
-	def initialize(options={})
-		@name = options.fetch(:name)
-		@fave_color = options.fetch(:fave_color)
-		@fave_food = options.fetch(:fave_food)
+	def initialize(name = 'Elle Suzuki', fave_color = 'black', fave_food = 'candy')
+		@name = name
+		@fave_color = fave_color
+		@fave_food = fave_food
 	end
 end
--- spec/suzuki_spec.rb
+++ spec/suzuki_spec.rb
@@ -2,17 +2,19 @@ require 'rspec'
 require_relative "../lib/about-suzuki/suzuki.rb"
 
 describe Suzuki do
-	let(:suzuki) { Suzuki.new(:name => 'Elle S', :fave_food => 'sushi', :fave_color => 'black') }
+	context 'on instantiation' do
+		let(:suzuki) { Suzuki.new }
 
-	it 'should give me a name' do
-		suzuki.name.should == 'Elle S'		
-	end
+		it 'should give me a name' do
+			suzuki.name.should == 'Elle Suzuki'		
+		end
 
-	it 'should give me a favorite color' do
-		suzuki.fave_color.should == 'black' 
-	end
+		it 'should give me a favorite color' do
+			suzuki.fave_color.should == 'black' 
+		end
 
-	it 'should give me a favorite food' do
-		suzuki.fave_food.should == 'sushi'
+		it 'should give me a favorite food' do
+			suzuki.fave_food.should == 'candy'
+		end
 	end
 end
